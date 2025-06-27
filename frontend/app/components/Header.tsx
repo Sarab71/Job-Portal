e5d835980b482bfda7249c8760f6ae3cb5257100
Sarabnoor Singh <singhsarabnoor@gmail.com>
2025-06-27 18:44:31 +0530

    Header fixed

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,15 +1,40 @@
 'use client';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import {AuthContext } from '../context/AuthContext'; 
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
   const { isLoggedIn, user, logout } = useContext(AuthContext);
 
   const [menuOpen, setMenuOpen] = React.useState(false);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
   const handleMenuClose = () => setMenuOpen(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setMenuOpen(false);
+      }
+    };
+
+    if (menuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuOpen]);
+
 
   return (
     <header className="bg-gray-800 text-white">
@@ -21,7 +46,8 @@ const Header = () => {
 
         {/* Hamburger */}
         <button
-          className="md:hidden focus:outline-none z-20"
+          ref={buttonRef}
+          className="md:hidden focus:outline-none z-20 cursor-pointer"
           onClick={toggleMenu}
           aria-label="Toggle navigation"
         >
@@ -32,9 +58,11 @@ const Header = () => {
 
         {/* Menu */}
         <div
-          className={`absolute md:static bg-gray-800 top-0 left-0 w-full md:w-auto md:flex items-center z-10 transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}
+          ref={menuRef}
+          className={`absolute ${menuOpen ? 'flex mt-[56px]' : 'hidden'} md:static bg-gray-800 top-0 left-0 w-full md:w-auto md:flex items-center z-10 transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'
+            } md:translate-x-0`}
         >
-          <ul className="flex flex-col md:flex-row md:space-x-6 mt-16 md:mt-0">
+          <ul className="flex flex-col md:flex-row md:space-x-6 mt-1 md:mt-0">
             <li>
               <Link href="/" className="block px-4 py-2 hover:bg-gray-700" onClick={handleMenuClose}>
                 Home
